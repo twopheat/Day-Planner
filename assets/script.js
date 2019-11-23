7e7b1661f0f4071d7d58357c290ce72ccdf9781a
twopheat <twopheat@gmail.com>
2019-11-23 01:19:04 -0800

    all fields editable, and store locally! loop tmw

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,103 @@ $(document).ready(function() {
 // This function handles save button events
 $("#save1").on("click", function(save) {
   save.preventDefault();
+  
   // This line grabs the input from the data field
-
   var getData = $("#data1").val().trim();
-
   // Adding data from the data field to local storage
   localStorage.setItem('#data1', JSON.stringify(getData));
-  $(".data1").get(0).contentEditable = "true";
+  $("#data1").get(0).contentEditable = "true";
+
 });
 
+$("#save2").on("click", function(save) {
+  save.preventDefault();
+  
+  // This line grabs the input from the data field
+  var getData = $("#data2").val().trim();
+  // Adding data from the data field to local storage
+  localStorage.setItem('#data2', JSON.stringify(getData));
+  $("#data2").get(0).contentEditable = "true";
+
+});
+
+$("#save3").on("click", function(save) {
+  save.preventDefault();
+  
+  // This line grabs the input from the data field
+  var getData = $("#data3").val().trim();
+  // Adding data from the data field to local storage
+  localStorage.setItem('#data3', JSON.stringify(getData));
+  $("#data3").get(0).contentEditable = "true";
+
+});
+
+$("#save4").on("click", function(save) {
+  save.preventDefault();
+  
+  // This line grabs the input from the data field
+  var getData = $("#data4").val().trim();
+  // Adding data from the data field to local storage
+  localStorage.setItem('#data4', JSON.stringify(getData));
+  $("#data4").get(0).contentEditable = "true";
+
+});
+
+$("#save5").on("click", function(save) {
+  save.preventDefault();
+  
+  // This line grabs the input from the data field
+  var getData = $("#data5").val().trim();
+  // Adding data from the data field to local storage
+  localStorage.setItem('#data5', JSON.stringify(getData));
+  $("#data5").get(0).contentEditable = "true";
+
+});
+
+$("#save6").on("click", function(save) {
+  save.preventDefault();
+  
+  // This line grabs the input from the data field
+  var getData = $("#data6").val().trim();
+  // Adding data from the data field to local storage
+  localStorage.setItem('#data6', JSON.stringify(getData));
+  $("#data6").get(0).contentEditable = "true";
+
+});
+
+$("#save7").on("click", function(save) {
+  save.preventDefault();
+  
+  // This line grabs the input from the data field
+  var getData = $("#data7").val().trim();
+  // Adding data from the data field to local storage
+  localStorage.setItem('#data7', JSON.stringify(getData));
+  $("#data7").get(0).contentEditable = "true";
+
+});
+
+$("#save8").on("click", function(save) {
+  save.preventDefault();
+  
+  // This line grabs the input from the data field
+  var getData = $("#data8").val().trim();
+  // Adding data from the data field to local storage
+  localStorage.setItem('#data8', JSON.stringify(getData));
+  $("#data8").get(0).contentEditable = "true";
+
+});
+
+$("#save9").on("click", function(save) {
+  save.preventDefault();
+  
+  // This line grabs the input from the data field
+  var getData = $("#data9").val().trim();
+  // Adding data from the data field to local storage
+  localStorage.setItem('#data9', JSON.stringify(getData));
+  $("#data9").get(0).contentEditable = "true";
+
+});
+//Below is experimental loop element creation hopefully
 /*buildElements();
 
 
