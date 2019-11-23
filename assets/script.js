2e51e7844327b111758a3312512df930c809d4ad
twopheat <twopheat@gmail.com>
2019-11-23 00:56:17 -0800

    Attempting to loop and create all El. will try tmw

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,42 @@
 $(document).ready(function() {
-var dataString
-var getData
-// This function handles events where a movie button is clicked
-$("#saveBtn").on("click", function(save) {
+// Declare array for time of day 
+//var timeFrame = [9,10,11,12,1,2,3,4,5,];
+
+// This function handles save button events
+$("#save1").on("click", function(save) {
   save.preventDefault();
-  // This line grabs the input from the textbox
-  var getData = $("#dailyData").val().trim();
+  // This line grabs the input from the data field
 
-  // Adding data from the textbox to local storage
-  localStorage.setItem('#dailyData', JSON.stringify(getData));
-  $("#dailyData").get(0).contentEditable = "true";
+  var getData = $("#data1").val().trim();
 
-  
-  
+  // Adding data from the data field to local storage
+  localStorage.setItem('#data1', JSON.stringify(getData));
+  $(".data1").get(0).contentEditable = "true";
 });
+
+/*buildElements();
+
+
+
+function buildElements() {
+
+  var time = "<button></button>";
+  
+  var data = "<input></input>";
+  var save = "<input></input>";     
+  
+  var text = "";
+  var i;
+
+  $(".spacer").after(time);
+  $("form").after(data, save);
+
+
+  
+  };
+  for (i = 0; i <timeFrame ; i++) {
+    text += timeFrame[i] + "<br>";
+    buildElements(i);
+    //println(timeFrame[i]);
+  };*/
 });
