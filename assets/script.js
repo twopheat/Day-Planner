4292d8312cf041788eda5be55bd74c620fd3a9c7
twopheat <twopheat@gmail.com>
2019-11-22 23:38:51 -0800

    SUCCESS, can input,save, and store!

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
+$(document).ready(function() {
+var dataString
+var getData
 // This function handles events where a movie button is clicked
-$("#nineData").on("click", function(save) {
+$("#saveBtn").on("click", function(save) {
   save.preventDefault();
   // This line grabs the input from the textbox
-  var nineData = $("#nineData").val().trim();
+  var getData = $("#dailyData").val().trim();
 
-  // Adding movie from the textbox to our array
-  movies.push(movie);
+  // Adding data from the textbox to console log
+  localStorage.setItem('#dailyData', JSON.stringify(getData));
+  
 
   // Calling renderButtons which handles the processing of our movie array
-  renderButtons();
+  
+});
 });
