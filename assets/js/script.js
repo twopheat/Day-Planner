e8df429a1c6d418f0be6ebe408546b04191e1a46
twopheat <twopheat@gmail.com>
2019-11-24 01:02:14 -0800

    FIXED* PLEASE GRADE THIS ONE!Same as 11:00!restord

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,191 +1,132 @@
 $(document).ready(function() {
-
-  //--------------------------------------------
-
-var now
-var today = moment().format("MMM Do YYYY");
-var hour = moment().format("HH");
-
-var aFter = moment().isAfter(now, "hour");
-var beFore = moment().isBefore(now, "hour");
-var isNow = moment().isSame(now, "hour");
-
-function getNow() {
-  if (hour > 12){
-    now = (hour-12);
-  }
-  else {
-    now == hour;
-  }
-}
-getNow();
-
-
-/*function dueDate() {
-  if (aFter) {
-    $("data").addClass("bg-dark");
-  }
-  if (beFore) {
-    $("data").addClass("bg-success");
-  }
-  if (isNow) {
-    $("data").addClass("bg-danger");
-  }
-  };*/
+  // Declare array for time of day 
+  //var timeFrame = [9,10,11,12,1,2,3,4,5,];
   
-
-// These functions operate the storage of the input/display fields
-$("#save1").on("click", function(save) {
-  save.preventDefault();
+  // This function handles save button events
+  $("#save1").on("click", function(save) {
+    save.preventDefault();
+    
+    // This line grabs the input from the data field
+    var getData = $("#data1").val().trim();
+    // Adding data from the data field to local storage
+    localStorage.setItem('#data1', JSON.stringify(getData));
+    $("#data1").get(0).contentEditable = "true";
+  });
+  $('#data1').val(localStorage.getItem('#data1').replace(/"/g, ''));
   
-  // This line grabs the input from the data field
-  var getData = $("#data1").val().trim();
-  // Adding data from the data field to local storage
-  localStorage.setItem('#data1', JSON.stringify(getData));
-  //make field editable
-  $("#data1").get(0).contentEditable = "true";
-});
-$('#data1').val(localStorage.getItem('#data1').replace(/"/g, ''));
-
-//----------------------------------------------------------
-$("#save2").on("click", function(save) {
-  save.preventDefault();
+  $("#save2").on("click", function(save) {
+    save.preventDefault();
+    
+    // This line grabs the input from the data field
+    var getData = $("#data2").val().trim();
+    // Adding data from the data field to local storage
+    localStorage.setItem('#data2', JSON.stringify(getData));
+    $("#data2").get(0).contentEditable = "true";
   
-  // This line grabs the input from the data field
-  var getData = $("#data2").val().trim();
-  // Adding data from the data field to local storage
-  localStorage.setItem('#data2', JSON.stringify(getData));
-  $("#data2").get(0).contentEditable = "true";
-});
-$('#data2').val(localStorage.getItem('#data2').replace(/"/g, ''));
-//----------------------------------------------------------
-$("#save3").on("click", function(save) {
-  save.preventDefault();
+  });
+  $('#data2').val(localStorage.getItem('#data2').replace(/"/g, ''));
   
-  // This line grabs the input from the data field
-  var getData = $("#data3").val().trim();
-  // Adding data from the data field to local storage
-  localStorage.setItem('#data3', JSON.stringify(getData));
-  $("#data3").get(0).contentEditable = "true";
-});
-$('#data3').val(localStorage.getItem('#data3').replace(/"/g, ''));
-//----------------------------------------------------------
-$("#save4").on("click", function(save) {
-  save.preventDefault();
+  $("#save3").on("click", function(save) {
+    save.preventDefault();
+    
+    // This line grabs the input from the data field
+    var getData = $("#data3").val().trim();
+    // Adding data from the data field to local storage
+    localStorage.setItem('#data3', JSON.stringify(getData));
+    $("#data3").get(0).contentEditable = "true";
   
-  // This line grabs the input from the data field
-  var getData = $("#data4").val().trim();
-  // Adding data from the data field to local storage
-  localStorage.setItem('#data4', JSON.stringify(getData));
-  $("#data4").get(0).contentEditable = "true";
-});
-$('#data4').val(localStorage.getItem('#data4').replace(/"/g, ''));
-//----------------------------------------------------------
-
-$("#save5").on("click", function(save) {
-  save.preventDefault();
+  });
+  $('#data3').val(localStorage.getItem('#data3').replace(/"/g, ''));
   
-  // This line grabs the input from the data field
-  var getData = $("#data5").val().trim();
-  // Adding data from the data field to local storage
-  localStorage.setItem('#data5', JSON.stringify(getData));
-  //make field editable
-  $("#data5").get(0).contentEditable = "true";
-});
-$('#data5').val(localStorage.getItem('#data5').replace(/"/g, ''));
-
-//----------------------------------------------------------
-$("#save6").on("click", function(save) {
-  save.preventDefault();
+  $("#save4").on("click", function(save) {
+    save.preventDefault();
+    
+    // This line grabs the input from the data field
+    var getData = $("#data4").val().trim();
+    // Adding data from the data field to local storage
+    localStorage.setItem('#data4', JSON.stringify(getData));
+    $("#data4").get(0).contentEditable = "true";
   
-  // This line grabs the input from the data field
-  var getData = $("#data6").val().trim();
-  // Adding data from the data field to local storage
-  localStorage.setItem('#data6', JSON.stringify(getData));
-  $("#data6").get(0).contentEditable = "true";
-
-});
-$('#data6').val(localStorage.getItem('#data6').replace(/"/g, ''));
-//----------------------------------------------------------
-$("#save7").on("click", function(save) {
-  save.preventDefault();
+  });
+  $('#data4').val(localStorage.getItem('#data4').replace(/"/g, ''));
   
-  // This line grabs the input from the data field
-  var getData = $("#data7").val().trim();
-  // Adding data from the data field to local storage
-  localStorage.setItem('#data7', JSON.stringify(getData));
-  $("#data7").get(0).contentEditable = "true";
-});
-$('#data7').val(localStorage.getItem('#data7').replace(/"/g, ''));
-//----------------------------------------------------------
-$("#save8").on("click", function(save) {
-  save.preventDefault();
+  $("#save5").on("click", function(save) {
+    save.preventDefault();
+    
+    // This line grabs the input from the data field
+    var getData = $("#data5").val().trim();
+    // Adding data from the data field to local storage
+    localStorage.setItem('#data5', JSON.stringify(getData));
+    $("#data5").get(0).contentEditable = "true";
   
-  // This line grabs the input from the data field
-  var getData = $("#data8").val().trim();
-  // Adding data from the data field to local storage
-  localStorage.setItem('#data8', JSON.stringify(getData));
-  $("#data8").get(0).contentEditable = "true";
-});
-$('#data8').val(localStorage.getItem('#data8').replace(/"/g, ''));
-//------------------------------------------------------------
-$("#save9").on("click", function(save) {
-  save.preventDefault();
+  });
+  $('#data5').val(localStorage.getItem('#data5').replace(/"/g, ''));
   
-  // This line grabs the input from the data field
-  var getData = $("#data9").val().trim();
-  // Adding data from the data field to local storage
-  localStorage.setItem('#data9', JSON.stringify(getData));
-  $("#data9").get(0).contentEditable = "true";
-});
-$('#data9').val(localStorage.getItem('#data9').replace(/"/g, ''));
-
-
-
-
-
-
-document.getElementById("timeDate").innerHTML = today;
-console.log(today);
-console.log(now);
-console.log(aFter);
-console.log(beFore);
-console.log(isNow);
-
-
-
-//Below is experimental loop element creation hopefully (and others)
-/*buildElements();
-
-function dueDate() {
-  if (time > now) {
-      $("data").addClass("bg-dark");
-  }
-  if (time < now) {
-      $("data").addClass("bg-light");
-  }
-};
-
-
-function buildElements() {
-
-  var time = "<button></button>";
+  $("#save6").on("click", function(save) {
+    save.preventDefault();
+    
+    // This line grabs the input from the data field
+    var getData = $("#data6").val().trim();
+    // Adding data from the data field to local storage
+    localStorage.setItem('#data6', JSON.stringify(getData));
+    $("#data6").get(0).contentEditable = "true";
   
-  var data = "<input></input>";
-  var save = "<input></input>";     
+  });
+  $('#data6').val(localStorage.getItem('#data6').replace(/"/g, ''));
   
-  var text = "";
-  var i;
-
-  $(".spacer").after(time);
-  $("form").after(data, save);
-
-
+  $("#save7").on("click", function(save) {
+    save.preventDefault();
+    
+    // This line grabs the input from the data field
+    var getData = $("#data7").val().trim();
+    // Adding data from the data field to local storage
+    localStorage.setItem('#data7', JSON.stringify(getData));
+    $("#data7").get(0).contentEditable = "true";
   
-  };
-  for (i = 0; i <timeFrame ; i++) {
-    text += timeFrame[i] + "<br>";
-    buildElements(i);
-    //println(timeFrame[i]);
-  };*/
-});
+  });
+  $('#data7').val(localStorage.getItem('#data7').replace(/"/g, ''));
+  
+  $("#save8").on("click", function(save) {
+    save.preventDefault();
+    
+    // This line grabs the input from the data field
+    var getData = $("#data8").val().trim();
+    // Adding data from the data field to local storage
+    localStorage.setItem('#data8', JSON.stringify(getData));
+    $("#data8").get(0).contentEditable = "true";
+  
+  });
+  $('#data8').val(localStorage.getItem('#data8').replace(/"/g, ''));
+  
+  $("#save9").on("click", function(save) {
+    save.preventDefault();
+    
+    // This line grabs the input from the data field
+    var getData = $("#data9").val().trim();
+    // Adding data from the data field to local storage
+    localStorage.setItem('#data9', JSON.stringify(getData));
+    $("#data9").get(0).contentEditable = "true";
+  
+  });
+  $('#data9').val(localStorage.getItem('#data9').replace(/"/g, ''));
+  
+  //Below is experimental loop element creation hopefully
+  /*buildElements();
+  function buildElements() {
+    var time = "<button></button>";
+    
+    var data = "<input></input>";
+    var save = "<input></input>";     
+    
+    var text = "";
+    var i;
+    $(".spacer").after(time);
+    $("form").after(data, save);
+    
+    };
+    for (i = 0; i <timeFrame ; i++) {
+      text += timeFrame[i] + "<br>";
+      buildElements(i);
+      //println(timeFrame[i]);
+    };*/
+  });
